371bd05d10ee4ae89738f666f9a46a95b3d66de6
awillia2 <aaron.williams@mvnu.edu>
2020-08-11 07:53:19 -0400

    Fixed color in uncleared screening email

--- resources/views/emails/screenings/uncleared.blade.php
+++ resources/views/emails/screenings/uncleared.blade.php
@@ -8,6 +8,6 @@ View Screening
 | {{ $user->name }} ({{ $user->erp_id }}) |  |
 | --- | :---: |
 @foreach ($answers as $answer)
-| {{ $answer->question->text }} | @if($answer->value > 0) <span style="color:#5cb85c;">Yes</span> @else <span style="color:#d9534f;">No</span> @endif |
+| {{ $answer->question->text }} | @if($answer->value > 0) <span style="color:#d9534f;">Yes</span> @else <span style="color:#5cb85c;">No</span> @endif |
 @endforeach
 @endcomponent
